6769ef2012ac69848cf2bf8892f6ced6148ad2b3
Thomasvtf <thomas.vargas899@gmail.com>
2026-08-25 13:28:28 -0500

    Register V1

--- vestahome-prototype/src/App.jsx
+++ vestahome-prototype/src/App.jsx
@@ -1,26 +1,29 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
-
 import Login from './pages/Login'
+import Register from "./pages/Register"
 
 function App() {
-
   const [paso, setPaso] = useState(1);
 
   
   return (
     <>
+      <Register/>
+
       <div className='pagina-inicio'>
         {paso === 1 && (
           <Login 
           siguiente = {() => setPaso(2)}/>
         )}
       </div>
-      
-      <div className='interfaz-global'>
-        <Navbar/>
-      </div>
+      {paso !== 1 && (
+        <div className='interfaz-global'>
+          <Navbar/>
+        </div>
+
+      )}
     </>
   )
 }
---
+++ vestahome-prototype/src/pages/Register.jsx
@@ -0,0 +1,22 @@
+function Register(){
+
+    return(
+        <div className="contenedor-register">
+            <div className="card-form">
+                <form noValidate>
+                    <div className="txt-register">
+                        <h2>Únete a VestaHome</h2>
+                        <p>
+                            Entra al futuro de la gestión inmobiliaria premium.
+                        </p>
+                    </div>
+                    <div className="grupo-registrer">
+                        
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Register;
